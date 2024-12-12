6bf0fee9baf9b79a1c997cbb9d082a70541a7055
Oscar Cortez <oscarcortez@MacBook-Air-de-Oscar.local>
2024-12-12 13:37:09 -0600

    feat: add reducer to fetch data from api

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -1,3 +1,6 @@
+import axios from "axios";
+import { useReducer, useEffect } from "react";
+
 enum ActionType {
     FETCH_START = "FETCH_START",
     FETCH_SUCCESS = "FETCH_SUCCESS",
@@ -16,6 +19,17 @@ interface Action<T> {
     error?: string;
 }
 
+export enum ApiConstants {
+    PHOTO_API_URL = "http://localhost:3000/externalapi/photos",
+}
+  
+
+// Initial state for reducers
+const initialState = {
+    data: [] as any[],
+    loading: false,
+    error: null,
+  };
 
 const apiReducer = <T,>(state: State<T>, action: Action<T>): State<T> => {
     switch (action.type) {
@@ -37,3 +51,28 @@ const apiReducer = <T,>(state: State<T>, action: Action<T>): State<T> => {
             return state;
     }
 };
+
+export const useFetchPhotos = (filter: string) => {
+    const [state, dispatch] = useReducer(apiReducer, initialState);
+  
+    useEffect(() => {
+      const fetchData = async () => {
+        dispatch({ type: ActionType.FETCH_START });
+        try {
+          const url = `${ApiConstants.PHOTO_API_URL}?filter=${filter}`;
+          const response = await axios.get(url);
+          dispatch({ type: ActionType.FETCH_SUCCESS, payload: response.data });
+        } catch (err) {
+          dispatch({ type: ActionType.FETCH_ERROR, error: "Error fetching photos" });
+          console.error("Error fetching photos:", err);
+        }
+      };
+  
+      if (filter) {
+        fetchData();
+      }
+    }, [filter]);
+  
+    return state;
+  };
+
